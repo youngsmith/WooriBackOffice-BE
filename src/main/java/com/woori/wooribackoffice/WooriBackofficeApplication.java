f807bbd421bfa84b436cf3c9dd54acb8710c3a45
youngsmith <yyjjang9@naver.com>
2021-06-21 19:51:40 +0900

    f

---
+++ src/main/java/com/woori/wooribackoffice/WooriBackofficeApplication.java
@@ -0,0 +1,13 @@
+package com.woori.wooribackoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriBackofficeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooriBackofficeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/woori/wooribackoffice/WooriBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woori.wooribackoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriBackofficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
