4c2a176f5d0dd08e06438c9347ea9d4f3b38d40a
youngsmith <yyjjang9@naver.com>
2021-07-12 16:04:19 +0900

    commit

--- src/main/java/com/woori/wooribackoffice/configuration/WebConfig.java
+++ src/main/java/com/woori/wooribackoffice/configuration/WebConfig.java
@@ -1,11 +1,8 @@
 package com.woori.wooribackoffice.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -13,13 +10,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*");
-    }
-
-    @Bean
-    public ViewResolver internalResourceViewResolver() {
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setPrefix("/static/");
-        bean.setSuffix(".html");
-        return bean;
     }
 }
