a081cc9ef3103a1f13ca3996d111ce1959dd43a9
youngsmith <yyjjang9@naver.com>
2021-07-13 04:15:17 +0900

    add comments

--- src/main/java/com/woori/wooribackoffice/domain/entity/User.java
+++ src/main/java/com/woori/wooribackoffice/domain/entity/User.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
 @NoArgsConstructor
-@Table(name = "member")
+@Table(name = "member")     // 테이블 명을 user 로 하면 보통 user 는 예약어이기 때문에 실행시 테이블을 찾을 수 없는 오류가 발생한다. member 테이블로 이름을 변경함
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
