4e3de64c613c28ff9408e0cde00dc7a64ecee47d
youngsmith <yyjjang9@naver.com>
2021-07-19 02:14:46 +0900

    commit

--- src/main/java/com/woori/wooribackoffice/domain/dto/response/ExaminationResponse.java
+++ src/main/java/com/woori/wooribackoffice/domain/dto/response/ExaminationResponse.java
@@ -1,10 +1,12 @@
 package com.woori.wooribackoffice.domain.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.woori.wooribackoffice.domain.entity.Examination;
 import com.woori.wooribackoffice.domain.entity.ExaminationCategory;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
 @Accessors(chain = true)
 public class ExaminationResponse {
     private Long id;
+    @JsonFormat(pattern = "YYYY-MM-dd")
     private LocalDate examinationDate;
     private String registrationNumber;
     private FarmResponse farmResponse;
